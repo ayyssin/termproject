3c5e2097a4742fef1fbb6ae8208bef3d1c5c3ba0
Zhansaya Maksut <maksutzh@gmail.com>
2020-11-12 16:13:38 +0600

    connect manager profile page + employees list, try of popup

--- src/main/webapp/manager-employee.js
+++ src/main/webapp/manager-employee.js
@@ -1,36 +1,140 @@
+let list = localStorageUtil.getProducts();
+let displayCards = [];
 
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+employees.forEach(({id, name, position, schedule, hours, salary, total_payroll}) => {
+    if(list.indexOf(id) != -1){
+        displayCards.push({id, name, position, schedule, hours, salary, total_payroll});
+    }
+});
 
-openModalButtons.forEach(button =>{
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+function runModal(employeeID){
+    var modal = document.getElementById("myModal" + " " + employeeID);
 
-overlay.addEventListener('click', () =>{
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal =>{
-        closeModal(modal)
-    })
-})
+    var btn = document.getElementById("myBtn" + " " + employeeID);
 
-closeModalButtons.forEach(button =>{
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
+    var span = document.getElementsByClassName("close" + " " + employeeID)[0];
 
-function openModal(modal) {
-    if(modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
-function closeModal(modal) {
-    if(modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
+
+//Script on appending/deleting bookings
+
+//we neeed to pass variables to this function so 
+//to make something like city.innerHTML = VARIABLE got from DB & user input
+//the id of each generated booking should be different and it passed to modal function as well as well
+function generateModal(employeeID, name, position, schedule, hours, salary, total_payroll){
+    let main = document.querySelector(".modals");
+    let node = document.createElement("div");
+    node.className += "modal";
+    node.id += ("myModal" + " " + employeeID);
+    main.appendChild(node);
+
+    let content = document.createElement("div");
+    content.className += "modal-content";
+    node.appendChild(content);
+
+    let body = document.createElement("div");
+    body.className += "modal-body";
+    content.appendChild(body);
+
+    let close = document.createElement("span");
+    close.className += ("close" + " " + employeeID);
+    close.innerHTML = "&times;"
+    body.appendChild(close);
+
+    let title= document.createElement("span");
+    title.className += ("staff-name" + " " + employeeID);
+    title.innerHTML = name
+    body.appendChild(title);
+
+    let edit = document.createElement("a");
+    edit.href = "edit.html"
+    edit.className += ("edit" + " " + employeeID);
+    body.appendChild(edit);
+
+    let info = document.createElement("div");
+    info.className += "info-body";
+    content.appendChild(info);
+
+    let positions = document.createElement("div");
+    positions.className += ("positions" + " " + employeeID);
+    positions.innerHTML = position
+    info.appendChild(positions);
+
+    let schedules = document.createElement("div");
+    schedules.className += ("schedules" + " " + employeeID);
+    schedules.innerHTML = schedule
+    info.appendChild(schedules);
+
+    let hour = document.createElement("div");
+    hour.className += ("hour" + " " + employeeID);
+    title.innerHTML = hours
+    info.appendChild(hour);
+
+    let salaries = document.createElement("div");
+    salaries.className += ("salaries" + " " + employeeID);
+    salaries.innerHTML = salary
+    info.appendChild(salaries);
+
+    let total = document.createElement("div");
+    total.className += ("total" + " " + employeeID);
+    total.innerHTML = total_payroll
+    info.appendChild(total);
+
+    let payroll = document.createElement("button");
+    payroll.className = "payroll"
+    payroll.innerHTML = "Make payroll";
+    content.appendChild(payroll);
 }
+
+displayCards.forEach(({id, name, img, position,  hours}) => {
+
+    let employeeID = id;
+
+    let main = document.querySelector("#rectangles");
+
+    let node = document.createElement("div");
+    node.className += ("rect" + " " + id);
+
+    let image = document.createElement("img");
+    image.setAttribute("src", img);
+
+    let title = document.createElement("div");
+    title.className += "card-title";
+    title.innerHTML = name;
+
+    let positions = document.createElement("div");
+    positions.className += "positions";
+    positions.innerHTML = position;
+
+    let hour = document.createElement("div");
+    hour.className += "hour";
+    hour.innerHTML = hours;
+
+    let moreDetails = document.createElement("button");
+    moreDetails.id += "myBtn" + " " + id;
+    moreDetails.innerHTML = "See more";
+
+    node.appendChild(image);
+    node.appendChild(title);
+    node.appendChild(positions);
+    node.appendChild(hour);
+    node.appendChild(moreDetails);
+    main.appendChild(node);
+
+    //we have to pass some variables to generate specific modals and make sure they're working
+    generateModal(id, name, position, schedule, hours, salary, total_payroll);
+    runModal(id);
+});
